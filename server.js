815b4119c96355925c90b77b07c0046136b78e6b
lolaodelola <damzcodes@gmail.com>
2020-10-20 11:14:17 +0100

    rm comments

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const http = require('http');
 const path = require('path');
